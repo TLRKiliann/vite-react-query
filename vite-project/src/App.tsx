43fde96e91c2c6d03040c9779018385f6ece4b29
TLRKiliann <philogenie@protonmail.com>
2023-04-20 23:39:18 +0200

    react-query & react-query-devtools setup

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,22 +1,28 @@
 import React from 'react'
+import {QueryClientProvider, QueryClient} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import RequestQuery from './pages/RequestQuery';
-import Traditionnal from './pages/Traditionnal';
+import RequestQueryRQ from './pages/RequestQueryRQ';
+import Traditional from './pages/Traditional';
 import './App.css'
+
+const queryClient = new QueryClient();
 
 const App:React.FC = () => {
   return (
     <div>
+      <QueryClientProvider client={queryClient}>
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/requestquery" element={<RequestQuery />} />
-        <Route path="/traditionnal" element={<Traditionnal />} />
+        <Route path="/requestqueryRQ" element={<RequestQueryRQ />} />
+        <Route path="/traditional" element={<Traditional />} />
       </Routes>
+      <ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
+      </QueryClientProvider>
     </div>
   )
 }
-
 export default App;
